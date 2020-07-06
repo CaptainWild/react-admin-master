aa197d1cd52023306012b6f35c21356e2cb150ea
captainwild <devservicedevall@gmail.com>
2020-07-06 23:59:27 +0800

    feat: build login page(UI)

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { Admin, Resource } from 'react-admin';
 // import restProvider from 'ra-data-simple-rest';
 import jsonServerProvider from 'ra-data-json-server';
+import authProvider from './components/authentication/authProvider';
 
 import { UserList, UserEdit, UserIcon } from './components/users';
 import { ProductList, ProductEdit, ProductShow, ProductCreate, ProductIcon } from './components/products';
 
 function App() {
   return (
-    <Admin dataProvider={jsonServerProvider('https://jsonplaceholder.typicode.com')}>
+    <Admin dataProvider={jsonServerProvider('https://jsonplaceholder.typicode.com')} authProvider={authProvider}>
       <Resource name='users' list={UserList} edit={UserEdit} icon={UserIcon} />
       <Resource name='posts' options={{ label: 'Products' }} list={ProductList} edit={ProductEdit} show={ProductShow} create={ProductCreate} icon={ProductIcon} />
     </Admin>
